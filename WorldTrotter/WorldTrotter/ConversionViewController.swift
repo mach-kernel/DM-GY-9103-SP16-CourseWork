ba30a3cc306d1344c0f044995a0178afc2ff45a8
David Stancu <David@172-16-14-61.DYNAPOOL.NYU.EDU>
2016-02-09 18:32:25 -0500

    chapters 3,4

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by David Stancu on 2/9/16.
+//  Copyright © 2016 David Stancu. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let value = Double(textField.text!) {
+            fahrenheitValue = value
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * 5/9
+        }
+        else {
+            return nil
+        }
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
+        let replacementTextHasDecimalSeparator = string.rangeOfString(".")
+        
+        if existingTextHasDecimalSeparator != nil &&
+            replacementTextHasDecimalSeparator != nil {
+                return false
+        }
+        else {
+            return true
+        }
+    }
+}
