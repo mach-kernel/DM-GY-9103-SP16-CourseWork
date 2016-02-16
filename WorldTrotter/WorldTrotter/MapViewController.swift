31d0c45115b5477b77dbc1784a0f9af96d1f43f9
David Stancu <David@Charcoal.local>
2016-02-15 20:18:53 -0500

    chapter 6 :wq wq wq pls help :wqqq q:qq;q;q;q;q;wqw;w;q;qwhywouldyoudothistome
    fix embarassing error

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by David Stancu on 2/15/16.
+//  Copyright © 2016 David Stancu. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view")
+    }
+}
