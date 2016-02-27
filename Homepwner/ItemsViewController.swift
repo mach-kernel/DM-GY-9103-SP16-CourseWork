4f069b9b01bdc907cd7fe54accae2e14e308c404
David Stancu <David@172-16-12-197.DYNAPOOL.NYU.EDU>
2016-02-27 18:28:59 -0500

    ch 11

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -18,6 +18,9 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left:0, bottom:0, right:0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 65
     }
     
     @IBAction func addNewItem(sender: AnyObject) {
@@ -71,11 +74,26 @@ class ItemsViewController: UITableViewController {
         itemStore.moveItemAtIndex(sourceIndexPath.row, toIndex: destinationIndexPath.row)
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style:.Value1, reuseIdentifier: "UITableViewCell")
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> ItemCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ItemCell", forIndexPath: indexPath) as! ItemCell
+        
         let item = itemStore.allItems[indexPath.row]
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
+        
+        
+        
+        if (item.valueInDollars >= 50) {
+            cell.valueLabel.textColor = UIColor.redColor()
+        }
+        else {
+            cell.valueLabel.textColor = UIColor.greenColor()
+        }
+        
+        cell.updateLabels()
+        
         return cell
     }
 }
